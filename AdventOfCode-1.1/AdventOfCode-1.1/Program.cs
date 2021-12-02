ce5f7805a2953dc0d35701739fd660eb3d896d9a
Georgio Bitar <georgiobitar1@hotmail.com>
2021-12-02 19:45:09 +0200

    Update Solution 1 and Solved Day 2

--- AdventOfCode-1.1/AdventOfCode-1.1/Program.cs
+++ AdventOfCode-1.1/AdventOfCode-1.1/Program.cs
@@ -4,4 +4,6 @@ using AdventOfCode_1._1;
 QuestionsSolver solver = new QuestionsSolver();
 
 //solver.Question11();
-solver.Question12();
+//solver.Question12();
+//solver.Question21();
+solver.Question22();
