c86b3af1d8963e63f6a86f228051c34dfa86ef50
Georgio Bitar <georgiobitar1@hotmail.com>
2021-12-04 00:00:02 +0200

    Solved Day 3 (needs optimization)

--- AdventOfCode-1.1/AdventOfCode-1.1/Program.cs
+++ AdventOfCode-1.1/AdventOfCode-1.1/Program.cs
@@ -6,4 +6,6 @@ QuestionsSolver solver = new QuestionsSolver();
 //solver.Question11();
 //solver.Question12();
 //solver.Question21();
-solver.Question22();
+//solver.Question22();
+//solver.Question31();
+solver.Question32();
--- AdventOfCode-1.1/AdventOfCode-1.1/QuestionsSolver.cs
+++ AdventOfCode-1.1/AdventOfCode-1.1/QuestionsSolver.cs
@@ -89,6 +89,91 @@ namespace AdventOfCode_1._1
 
             Console.WriteLine("Day 2 Question 2: " + horizontalPosition * depth);
         }
+
+        public void Question31()
+        {
+            int k = 0;
+            string gamma = "";
+            List<int> count = new List<int>();
+            System.IO.File.ReadLines(@"..\\..\\..\\inputDay3.txt").First().ToCharArray().ToList().ForEach(x => count.Add(int.Parse(x.ToString())));
+            
+            foreach (string line in System.IO.File.ReadLines(@"..\\..\\..\\inputDay3.txt").Skip(1))
+            {
+                for(int i=0; i<count.Count; i++)
+                {
+                    count[i] += int.Parse(line[i].ToString());                
+                }
+                k++;
+            }
+            for(int j=0; j<count.Count; j++)
+            {
+                gamma += (count[j] > k/2) ?  "1" : "0";
+            }
+            Console.WriteLine("Power Consumption: " + Convert.ToInt32(gamma, 2)*Convert.ToInt32(gamma.Replace("1", "2").Replace("0", "1").Replace("2", "0"), 2));
+        }
+
+        public void Question32()
+        {
+            List<string> lines = System.IO.File.ReadLines(@"..\\..\\..\\inputDay3.txt").ToList();
+            Int64 oxygenGeneratorRating = 0;
+            Int64 co2ScrubberRating = 0;
+            int k = 0;
+            List<int> count = new List<int>(){ 0 };
+            while(oxygenGeneratorRating == 0)
+            {
+                foreach(string line in lines)
+                {
+                    count[k] += int.Parse(line[k].ToString());
+                }
+                if(count[k]>= int.Parse(lines.Count.ToString()) / 2.0)
+                {
+                    lines.RemoveAll(x => x[k] == '0');
+                }
+                else {
+                    lines.RemoveAll(x => x[k] == '1');
+                }
+
+                if (lines.Count == 1)
+                {
+                    oxygenGeneratorRating = Convert.ToInt64(String.Join("",lines.First()),2);
+                }
+                else
+                {
+                    k++;
+                    count.Add(0);
+                }
+            }
+
+            lines = System.IO.File.ReadLines(@"..\\..\\..\\inputDay3.txt").ToList();
+            count = new List<int>() { 0 };
+            k = 0;
+            while (co2ScrubberRating == 0)
+            {
+                foreach (string line in lines)
+                {
+                    count[k] += int.Parse(line[k].ToString());
+                }
+                if (count[k] >= int.Parse(lines.Count.ToString()) / 2.0)
+                {
+                    lines.RemoveAll(x => x[k] == '1');
+                }
+                else
+                {
+                    lines.RemoveAll(x => x[k] == '0');
+                }
+
+                if (lines.Count == 1)
+                {
+                    co2ScrubberRating = Convert.ToInt64(String.Join("", lines.First()), 2);
+                }
+                else
+                {
+                    k++;
+                    count.Add(0);
+                }
+            }
+            Console.WriteLine("Life Support Rating: " + oxygenGeneratorRating * co2ScrubberRating);
+        }
     }
 }
 
